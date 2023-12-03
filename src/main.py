d8115c260e641ae75a6cd05bbecca9d68d274ff8
tomasztaw <tomasz.wegrzyn.wro@gmail.com>
2023-12-03 15:23:54 +0100

    Making a board and arranging the figures

--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@ import pygame
 import sys
 
 from const import *
+from game import Game
 
 class Main:
 
@@ -9,18 +10,24 @@ class Main:
         pygame.init()
         self.screen = pygame.display.set_mode((WIDTH, HEIGHT))
         pygame.display.set_caption('Chess')
-
+        self.game = Game()
 
     def mainloop(self):
+
+        screen = self.screen
+        game = self.game
+
         while True:
+            game.show_bg(screen)
+            game.show_pieces(screen)
+
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit()
                     sys.exit()
 
-
-
             pygame.display.update()
+
 
 main = Main()
 main.mainloop()
